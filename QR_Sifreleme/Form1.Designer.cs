ef3fe0abe77d1e3176543a30c23ea291684154d2
Hüseyin Eren Çeykel <huseyinerenceykel@gmail.com>
2023-09-01 01:55:29 +0300

    QR OLUŞTURMA VE ÇÖZÜMLEME PROJE ÖRNEĞİ
    C# ile Windows Form kullanarak yaptığım QR Oluşturma ve Çözümleme proje örneği

---
+++ QR_Sifreleme/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace QR_Sifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtMetin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnOlustur = new System.Windows.Forms.Button();
+            this.BtnCozumle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(363, 279);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtMetin
+            // 
+            this.TxtMetin.Location = new System.Drawing.Point(66, 9);
+            this.TxtMetin.Name = "TxtMetin";
+            this.TxtMetin.Size = new System.Drawing.Size(309, 26);
+            this.TxtMetin.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Metin";
+            // 
+            // BtnOlustur
+            // 
+            this.BtnOlustur.Location = new System.Drawing.Point(16, 41);
+            this.BtnOlustur.Name = "BtnOlustur";
+            this.BtnOlustur.Size = new System.Drawing.Size(138, 44);
+            this.BtnOlustur.TabIndex = 3;
+            this.BtnOlustur.Text = "Oluştur";
+            this.BtnOlustur.UseVisualStyleBackColor = true;
+            this.BtnOlustur.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnCozumle
+            // 
+            this.BtnCozumle.Location = new System.Drawing.Point(237, 41);
+            this.BtnCozumle.Name = "BtnCozumle";
+            this.BtnCozumle.Size = new System.Drawing.Size(138, 44);
+            this.BtnCozumle.TabIndex = 4;
+            this.BtnCozumle.Text = "Çözümle";
+            this.BtnCozumle.UseVisualStyleBackColor = true;
+            this.BtnCozumle.Click += new System.EventHandler(this.BtnCozumle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 382);
+            this.Controls.Add(this.BtnCozumle);
+            this.Controls.Add(this.BtnOlustur);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtMetin);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TxtMetin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnOlustur;
+        private System.Windows.Forms.Button BtnCozumle;
+    }
+}
+
---
+++ QR_Sifreleme/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QR_Sifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder enc = new QRCodeEncoder();
+            pictureBox1.Image = enc.Encode(TxtMetin.Text);            
+        }
+
+        private void BtnCozumle_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder dnc = new QRCodeDecoder();
+            TxtMetin.Text=dnc.Decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap));
+        }
+    }
+}
